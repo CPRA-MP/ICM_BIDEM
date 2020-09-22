d43c99f2ff081b370e230731ae32ff472e286b5b
ddileonardo <39922065+ddileonardo@users.noreply.github.com>
2020-09-22 16:38:58 -0500

    Update interpolate_to_fixed_for_ICM.py

--- interpolate_to_fixed_for_ICM.py
+++ interpolate_to_fixed_for_ICM.py
@@ -12,6 +12,9 @@ import pandas as pd #used here for reading csv files
 import numpy as np #used for creating arrays of the csv files
 from scipy.interpolate import griddata #used for the interpolation
 
+#Define the save location
+save_loc = 'xyz output name' #output name and file path
+
 #Read the data files
 model_results = 'model results filename' #This is the filepath and filename of the model results
 results = pd.read_csv(model_results,delim_whitespace=True,names=['x','y','z','trans']) #Read the model results file
@@ -27,3 +30,10 @@ fixed_grid_array = np.vstack((fixed_grid.XCoord,fixed_grid.YCoord)).T  #array of
 
 #Interpolation 
 interp_results = griddata(results_xy,results.z,fixed_grid_array,method='linear') #interp_results is the z value of the model results interpolated to the fixed output grid point
+
+#Write the x, y, z values from the fixed grid
+#File format is: point index, X, Y, Z value
+#There is one headerline
+#There is one file for each BI-DEM Region
+write = pd.DataFrame(data = np.hstack((fixed_grid_array,np.expand_dims(interp_results,axis=1))),columns=['x','y','z'])
+write.to_csv(save_loc)
