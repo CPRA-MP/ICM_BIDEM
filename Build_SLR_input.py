811986474caed9ee5b08557cb4f3b58fe219d0c5
Maddie Foster-Martinez <49647692+fostinez@users.noreply.github.com>
2020-10-01 22:03:00 -0500

    Update Build_SLR_input.py
    -Removes differencing between the years to get the rate
    -Fits the annual mean data to a quadratic
    -Takes a derivative to get the rate

--- Build_SLR_input.py
+++ Build_SLR_input.py
@@ -14,28 +14,22 @@ ESLR_out_file = r'%s\%s' % (ESLR_out_file_path, ESLR_out_file_name)
 
 first_year = int(tide_data[0][0][0:4])
 last_year = first_year+50 # alternative if it's not always 50 years: last_year = int(tide_data[-1][0][0:4])+1
+years = range(first_year, last_year)
 
-with open(ESLR_out_file, mode='w') as outf:
-    year_previous_mean_mm = 0
-    for year in range(first_year,last_year):
-        n = 0
-        annual_total = 0
-        for r in tide_data:
-            if int(r[0][0:4]) == year:
-                annual_total += float(r[3]) #4th column in TideData is Amerada Pass, LA
-                n+=1 #8760 hours or 8784 hours if leap year 
-        year_mean_mm = ((annual_total/n)*1000) #take the mean of the hourly data and convert from m to mm
-        year_ESLR_mm = year_mean_mm-year_previous_mean_mm #subtract previous year to get the rate
-        year_previous_mean_mm = year_mean_mm
-        if year == first_year: #extrapolate the SLR rate from year 2 to 3 to year 1 to 2; otherwise the SLR in year 1 is off
-            pass
-        elif year == first_year +1:
-            year2_ESLR_mm = year_ESLR_mm
-        elif year == first_year +2:
-            delta_SLR = year_ESLR_mm - year2_ESLR_mm
-            year1_ESLR_mm = year2_ESLR_mm - delta_SLR
-            outf.write('%s %s\n' % (first_year, year1_ESLR_mm))
-            outf.write('%s %s\n' % (first_year+1, year2_ESLR_mm))                
-            outf.write('%s %s\n' % (year, year_ESLR_mm))
-        else:
-            outf.write('%s %s\n' % (year, year_ESLR_mm))
+annual_mean_mm = [] 
+for year in range(first_year,last_year):
+    n = 0
+    annual_total = 0
+    for r in tide_data:
+        if int(r[0][0:4]) == year:
+            annual_total += float(r[3]) #4th column in TideData is Amerada Pass, LA
+            n+=1 #8760 hours or 8784 hours if leap year      
+    annual_mean_mm.append((annual_total/n)*1000) #take the mean of the hourly data and convert from m to mm
+p = np.polyfit(years, np.asarray(annual_mean_mm),2) #fit a quadratic to the annual mean data
+ESLR_rate_mmyr = (p[0]*2*years)+(p[1]) #take the first derivative and plug in years to get the rate
+            
+with open(ESLR_out_file, mode='w') as outf: 
+    for i in range(0,len(years)):
+        outf.write("%s %s\n" % (years[i],ESLR_rate_mmyr[i]))
+                            
+outf.close()     
