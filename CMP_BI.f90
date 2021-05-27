1166f2dee235a506b3f00b1e65da7078aa464c59
Eric White <47019383+edwhite12@users.noreply.github.com>
2021-05-27 10:46:34 -0500

    debug

--- CMP_BI.f90
+++ CMP_BI.f90
@@ -3434,7 +3434,7 @@ end subroutine auto_restoration
 
     do i=1,line-1
       read(10,'(A)') linestring
-      write(*,*) i,':',line,trim(linestring)
+      write(*,'(A)') trim(linestring)
     enddo
 
 1005 FORMAT(A13,L)
